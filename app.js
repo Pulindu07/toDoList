038875c636ea9e97ad8f7680b6b7fb715b29a7e0
Pulindu de Silva <pulindu@Pulindus-MacBook-Air.local>
2022-09-07 15:17:44 +0530

    Correct another spelling mistake

--- app.js
+++ app.js
@@ -134,13 +134,13 @@ app.post("/delete", function (req, res) {
 });
 
 let port = process.env.PORT;
+
 if (port == null || port == "") {
   port = 3000;
 }
 app.listen(port);
 
 app.listen(port, function () {
-
     console.log("Network Up and Running on port 3000");
 
 });
